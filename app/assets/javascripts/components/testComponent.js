ddf782fd5ee3556db496ddb9cb91f8b5286a3065
Adrian Chen <adrian.evangelion@gmail.com>
2016-07-28 16:09:40 +0100

    init commit

---
+++ app/assets/javascripts/components/testComponent.js
@@ -0,0 +1,10 @@
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("a", {className: "wut", ref: "link", href: "#", target: "_blank"}, 
+          "I am a link."
+        )
+      )
+    );
+  }
+});
---
+++ app/assets/javascripts/components/testComponent.jsx
@@ -0,0 +1,11 @@
+var testComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a className="wut" ref="link" href="#" target="_blank">
+          I am a link.
+        </a>
+      </div>
+    );
+  }
+});
